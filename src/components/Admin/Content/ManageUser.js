625e6814d5ec9335e52a2dd1abf524098d606a7e
LeDinhAnh <doanhle1032004@gmail.com>
2024-06-05 09:50:24 +0700

    Cập Nhật Danh Sách Users Khi Thêm Mới Thành Công

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,11 +1,25 @@
 import ModalCreateUser from "./ModalCreateUser";
 import "./ManageUser.scss";
 import { FcPlus } from "react-icons/fc";
-import { useState } from "react";
+
 import TableUser from "./TableUser";
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../../services/apiServices";
 const ManageUser = (props) => {
   const [showModalCreateUser, setShowModalCreateUser] = useState(false);
 
+  const [listUsers, setListUsers] = useState([]);
+  useEffect(() => {
+    fetchListUsers();
+  }, []);
+
+  const fetchListUsers = async () => {
+    let res = await getAllUsers();
+
+    if (res.data.EC === 0) {
+      setListUsers(res.data.DT);
+    }
+  };
   const handleShowHideModal = (value) => {
     setShowModalCreateUser(value);
   };
@@ -24,12 +38,13 @@ const ManageUser = (props) => {
           </button>
         </div>
         <div className="table-users-container">
-          <TableUser />
+          <TableUser listUsers={listUsers} />
         </div>
         <ModalCreateUser
           show={showModalCreateUser}
           // setShow={setShowModalCreateUser}
           setShow={handleShowHideModal}
+          fetchListUsers={fetchListUsers}
         />
       </div>
     </div>
